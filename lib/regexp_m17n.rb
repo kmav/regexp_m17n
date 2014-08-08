8acc2ee5b0bbeabdc2621346204e2f05f543d00c
Konstantinos Mavroudakis <kmavroudakis@gmail.com>
2014-08-08 12:11:06 +0300

    Using iconv - change return to true

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -2,7 +2,7 @@ require 'iconv'
 
 module RegexpM17N
   def self.non_empty?(str)
-    val = false
+    val = true
     puts (str.encoding.name)
     if str.encoding.name == "ASCII-8BIT"
       puts("found")
