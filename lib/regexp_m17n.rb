595f50ea023d56eff3cac8bf88fe84c30bbfae20
Konstantinos Mavroudakis <kmavroudakis@gmail.com>
2014-08-08 11:50:58 +0300

    Using iconv

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,6 +1,15 @@
+require 'iconv'
+
 module RegexpM17N
   def self.non_empty?(str)
-    str = str.encode "UTF-8"
-    str =~ /^.+$/
+    val = false
+    puts (str.encoding.name)
+    if str.encoding.name == "ASCII-8BIT"
+      puts("found")
+    else
+      str = Iconv.conv('UTF-8', str.encoding.name, str)
+      val = (str =~ /^.+$/)
+    end
+    return val
   end
 end
